776be0a8de05ba587b1f7fcb6f912bb34187e950
abdoachhoubi <aachhoub@gmail.com>
2022-11-16 11:35:51 +0100

    Fixed some responsivity issues

--- routes/Home/components/Banner/Banner.jsx
+++ routes/Home/components/Banner/Banner.jsx
@@ -14,37 +14,39 @@ const Banner = ({ width }) => {
 
   return (
     <section className="home__banner">
-      <motion.h1
-        className="banner__title"
-        initial={{ scale: 0.2, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 0.8, ease: "easeInOut" }}
-      >
-        Your guide to become a <span>successful</span> developer
-      </motion.h1>
-      <motion.p
-        className="banner__content"
-        initial={{ scale: 0.2, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 0.8, ease: "easeInOut" }}
-      >
-        Leetsource is where 1337 students can find any resource related to 1337
-        pool, 42 cursus, and programming generally
-      </motion.p>
-      <motion.div
-        initial={{ scale: 0.2, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 0.8, ease: "easeInOut" }}
-      >
-        <Link
-          className="banner__cta"
-          href="https://1337.ma"
-          target="_blank"
-          rel="noreferrer"
+      <article className="banner__hero">
+        <motion.h1
+          className="banner__title"
+          initial={{ scale: 0.2, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
         >
-          About 1337 School
-        </Link>
-      </motion.div>
+          Your guide to become a <span>successful</span> developer
+        </motion.h1>
+        <motion.p
+          className="banner__content"
+          initial={{ scale: 0.2, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          Leetsource is where 1337 students can find any resource related to
+          1337 pool, 42 cursus, and programming generally
+        </motion.p>
+        <motion.div
+          initial={{ scale: 0.2, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          <Link
+            className="banner__cta"
+            href="https://1337.ma"
+            target="_blank"
+            rel="noreferrer"
+          >
+            About 1337 School
+          </Link>
+        </motion.div>
+      </article>
       <motion.img
         initial={{ scale: 0.2, opacity: 0 }}
         whileInView={{ scale: 1, opacity: 1 }}
