45770e5c6190454d48531c6d6969e75cf95f66ce
abdoachhoubi <aachhoub@gmail.com>
2022-11-16 18:35:43 +0100

    Fixed nav title link error

--- routes/Home/components/Nav/src/MobNav.jsx
+++ routes/Home/components/Nav/src/MobNav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import Drawer from "./Drawer";
 import { Menu } from "react-feather";
 import { motion } from "framer-motion";
@@ -8,14 +9,16 @@ const MobNav = () => {
   return (
     <nav className="nav__mob">
       <div className="navbar">
-        <motion.h1
-          className="nav__title"
-          initial={{ scale: 0.2, opacity: 0 }}
-          whileInView={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 0.8, ease: "easeInOut" }}
-        >
-          <span>* </span>Leet Source
-        </motion.h1>
+        <Link href="/">
+          <motion.h1
+            className="nav__title"
+            initial={{ scale: 0.2, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.8, ease: "easeInOut" }}
+          >
+            <span>* </span>Leet Source
+          </motion.h1>
+        </Link>
         <motion.div
           className="menu__open"
           onClick={() => setMenu("in")}
