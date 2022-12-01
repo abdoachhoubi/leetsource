47d33cbaae3a5ebb8fff4b43f50c840578fa6d8c
abdoachhoubi <aachhoub@gmail.com>
2022-12-01 14:11:35 +0100

    Minor bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session} basePath="/custom-route/api/auth">
+    <SessionProvider session={session} basePath="/api/auth">
       <Component {...pageProps} />
     </SessionProvider>
   );
