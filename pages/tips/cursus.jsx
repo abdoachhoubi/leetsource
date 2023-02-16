f028ebfd85729c068e609153c3599039a6df3f2f
abdoachhoubi <aachhoub@gmail.com>
2023-02-16 19:49:58 +0100

    Rebase

---
+++ pages/tips/cursus.jsx
@@ -0,0 +1,213 @@
+import React, { useState, useEffect, useRef } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import pattern__res from "../../public/pattern.svg";
+import ct_banner from "../../public/ct_banner.png";
+
+const { src: pattern } = pattern__res;
+
+const bg__pattern = {
+  backgroundImage: `url(${pattern})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const Cursus = () => {
+  const [height, setHeight] = useState(0);
+  const image = useRef();
+  useEffect(() => {
+    setHeight(image.current.clientHeight);
+  }, [image]);
+  console.log(height);
+
+  return (
+    <>
+      <Head>
+        <title>Leet Source - Cursus Tips</title>
+        <meta
+          name="google-site-verification"
+          content="ovvmP3s_dWVp7bb05Bb8nGIrneErM1TaR8UDf2Yu32c"
+        />
+        <meta name="description" content="1337 Cursus Tips and Info" />
+        <meta
+          name="keywords"
+          content="source leet, leet source, source, leet, source 1337, 1337.ma,  1337, resource, cursus, pool, libft"
+        />
+
+        <meta property="og:url" content="https://source.leet.ma/cursus" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Leet Source" />
+        <meta property="og:description" content="1337 Cursus Tips and Info" />
+        <meta
+          property="og:image"
+          content="https://media.graphassets.com/YPq8cdnARUac586rauOS"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="source.leet.ma" />
+        <meta property="twitter:url" content="https://source.leet.ma/cursus" />
+        <meta name="twitter:title" content="Leet Source" />
+        <meta name="twitter:description" content="1337 Cursus Tips and Info" />
+        <meta
+          name="twitter:image"
+          content="https://media.graphassets.com/YPq8cdnARUac586rauOS"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="author" content="Astroboy" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="tips__header">
+        <nav className="navigation">
+          <h1 className="subheading">
+            <Link href="/">
+              <span className="primary">*</span>LeetSource
+            </Link>
+          </h1>
+          <ul className="nav__list">
+            <li className="text nav__list__item">
+              <Link href="/" className="nav__list__link">
+                Home
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/pool" className="nav__list__link">
+                Pool
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/cursus" className="nav__list__link">
+                Cursus
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/paths" className="nav__list__link">
+                Paths
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/resources" className="nav__list__link">
+                Resources
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__banner text-dark">
+          <div className="banner__pattern" style={bg__pattern}>
+            <p className="subheading mb-2">Cursus Tips</p>
+
+            <h1 className="heading">
+              To ensure an exceptional experience at 1337, consider adhering to
+              the following guidelines
+            </h1>
+          </div>
+        </div>
+        <div
+          className="image__container"
+          style={{ height: `calc(${height}px - 8vh + 4rem)` }}
+        >
+          <Image ref={image} src={ct_banner} className="ct_banner" />
+        </div>
+      </header>
+      <main className="tips__main">
+        <section className="description">
+          <h2 className="heading mb-2 mt-2 primary">Congratulations</h2>
+          <p className="text mb-4">
+            Congratulations on successfully passing the pool and welcome to 1337
+            as a new student!
+            <br />
+            The upcoming three to five years will undoubtedly be some of the
+            most rewarding and enriching experiences of your life.
+            <br />
+            <br />
+            <br />
+            As a new student, there are a few important things that you should
+            keep in mind.
+            <br />
+            <br />
+            Firstly, there is no distinction between new and old students at
+            1337.
+            <br />
+            You are encouraged to seek help from anyone and likewise, to offer
+            your assistance to others.
+            <br />
+            <br />
+            1337 expects all of its students to embody the values of humility,
+            respect, and honesty.
+            <br />
+            These virtues are essential for building a positive and
+            collaborative learning environment.
+            <br />
+            <br />
+            It's worth noting that the same pool rules still apply at 1337.
+            <br />
+            This means that you should continue to embrace the challenge of
+            pushing yourself beyond your limits and into uncharted territory.
+            <br />
+            <br />
+            Finally, as you progress through your projects, you will undoubtedly
+            gain a wealth of knowledge and experience.
+            <br />
+            Every milestone is a chance to explore new concepts and to further
+            develop your skills.
+            <br />
+            <br />
+            Congratulations on embarking on this exciting new journey, and we
+            wish you all the best for your future endeavours.
+          </p>
+          <h2 className="heading mb-2 mt-6 primary">Blackhole?</h2>
+          <p className="text">
+            Failure to effectively manage your time can result in being consumed
+            by the blackhole.
+            <br />
+            This metaphorical deadline should be taken seriously, as once it has
+            consumed you, there may be no returning.
+            <br />
+            <br />
+            Initially, you will have approximately 70 days until the blackhole's
+            deadline, but this can be extended through project validation.
+            <br />
+            <br />
+            As young adults, you hold full responsibility for your actions.
+          </p>
+        </section>
+      </main>
+      <footer className="home__footer">
+        <section className="copyright__line">
+          <p className="subheading mb-1">
+            <span className="primary">*</span>LeetSource
+          </p>
+          <p className="copyright text fw-300">
+            &copy; 2023 LeetSource, All rights reserved.
+          </p>
+        </section>
+        <section className="nav">
+          <Link className="text light" href="/">
+            Home
+          </Link>
+          <Link className="text light" href="/terms">
+            Terms and Conditions
+          </Link>
+          <Link className="text light" href="/about">
+            About
+          </Link>
+          <Link className="text light" href="/privacy">
+            Privacy Policy
+          </Link>
+          <Link className="text light" href="/blog">
+            Blog
+          </Link>
+          <Link className="text light" href="/cookie">
+            Cookie Policy
+          </Link>
+        </section>
+      </footer>
+    </>
+  );
+};
+
+export default Cursus;
---
+++ pages/tips/pool.jsx
@@ -0,0 +1,197 @@
+import React, { useState, useEffect, useRef } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import pattern__res from "../../public/pattern.svg";
+import pt_banner from "../../public/pt_banner.png";
+
+const { src: pattern } = pattern__res;
+
+const bg__pattern = {
+  backgroundImage: `url(${pattern})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const Pool = () => {
+  const [height, setHeight] = useState(0);
+  const image = useRef();
+  useEffect(() => {
+    setHeight(image.current.clientHeight);
+  }, [image]);
+  console.log(height);
+
+  return (
+    <>
+      <Head>
+        <title>Leet Source - Pool Tips</title>
+        <meta
+          name="google-site-verification"
+          content="ovvmP3s_dWVp7bb05Bb8nGIrneErM1TaR8UDf2Yu32c"
+        />
+        <meta name="description" content="1337 Pool Tips and Info" />
+        <meta
+          name="keywords"
+          content="source leet, leet source, source, leet, source 1337, 1337.ma,  1337, resource, cursus, pool, libft"
+        />
+
+        <meta property="og:url" content="https://source.leet.ma/tips/pool" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Leet Source" />
+        <meta property="og:description" content="1337 Pool Tips and Info" />
+        <meta
+          property="og:image"
+          content="https://media.graphassets.com/YPq8cdnARUac586rauOS"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="source.leet.ma" />
+        <meta
+          property="twitter:url"
+          content="https://source.leet.ma/tips/pool"
+        />
+        <meta name="twitter:title" content="Leet Source" />
+        <meta name="twitter:description" content="1337 Pool Tips and Info" />
+        <meta
+          name="twitter:image"
+          content="https://media.graphassets.com/YPq8cdnARUac586rauOS"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="author" content="Astroboy" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="tips__header">
+        <nav className="navigation">
+          <h1 className="subheading">
+            <Link href="/">
+              <span className="primary">*</span>LeetSource
+            </Link>
+          </h1>
+          <ul className="nav__list">
+            <li className="text nav__list__item">
+              <Link href="/" className="nav__list__link">
+                Home
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/pool" className="nav__list__link">
+                Pool
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/Pool" className="nav__list__link">
+                Pool
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/paths" className="nav__list__link">
+                Paths
+              </Link>
+            </li>
+            <li className="text nav__list__item">
+              <Link href="/resources" className="nav__list__link">
+                Resources
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__banner text-dark">
+          <div className="banner__pattern" style={bg__pattern}>
+            <p className="subheading mb-2">Pool Tips</p>
+
+            <h1 className="heading">
+              In order to ensure a successful and enjoyable pool, we recommend
+              following these simple tips.
+            </h1>
+          </div>
+        </div>
+        <div
+          className="image__container"
+          style={{ height: `calc(${height}px - 8vh + 4rem)` }}
+        >
+          <Image ref={image} src={pt_banner} className="pt_banner" />
+        </div>
+      </header>
+      <main className="tips__main">
+        <section className="description">
+          <h2 className="heading mb-2 mt-2 primary">Congratulations</h2>
+          <p className="text mb-4">
+            Congratulations on becoming a pisciner at 1337!
+            <br />
+            Get ready for an unforgettable month of learning and growth.
+          </p>
+          <h2 className="heading mb-2 mt-6 primary">General Tips</h2>
+          <ul className="list">
+            <li className="text list__item">
+              Manage your time effectively by creating a schedule.
+            </li>
+            <li className="text list__item">
+              Keep track of new information by taking notes.
+            </li>
+            <li className="text list__item">
+              Maintain good personal hygiene by showering regularly.
+            </li>
+            <li className="text list__item">
+              Prioritize sufficient sleep to maintain focus and productivity.
+            </li>
+            <li className="text list__item">
+              Avoid distractions caused by hunger by eating a nutritious diet.
+            </li>
+            <li className="text list__item">
+              Think before speaking to avoid damaging professional
+              relationships.
+            </li>
+            <li className="text list__item">
+              Show respect to all team members, including technical, cleaning,
+              and security staff.
+            </li>
+            <li className="text list__item">
+              Take care of equipment and facilities to maintain a safe and
+              comfortable work environment.
+            </li>
+            <li className="text list__item">
+              Foster positive relationships with colleagues by being friendly
+              and building professional connections.
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer className="home__footer">
+        <section className="copyright__line">
+          <p className="subheading mb-1">
+            <span className="primary">*</span>LeetSource
+          </p>
+          <p className="copyright text fw-300">
+            &copy; 2023 LeetSource, All rights reserved.
+          </p>
+        </section>
+        <section className="nav">
+          <Link className="text light" href="/">
+            Home
+          </Link>
+          <Link className="text light" href="/terms">
+            Terms and Conditions
+          </Link>
+          <Link className="text light" href="/about">
+            About
+          </Link>
+          <Link className="text light" href="/privacy">
+            Privacy Policy
+          </Link>
+          <Link className="text light" href="/blog">
+            Blog
+          </Link>
+          <Link className="text light" href="/cookie">
+            Cookie Policy
+          </Link>
+        </section>
+      </footer>
+    </>
+  );
+};
+
+export default Pool;
